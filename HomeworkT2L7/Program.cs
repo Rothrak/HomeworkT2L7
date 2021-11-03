ba784c5c1882aa058f28a421c049f59a790135b0
zaklak1998 <zaklak1998@wp.pl>
2021-11-03 13:52:29 +0100

    Add project files.

---
+++ HomeworkT2L7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkT2L7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
